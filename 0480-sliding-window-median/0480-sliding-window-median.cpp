7ce9a8c7dc6a292316d8b05ea0aa701fa6723d08
ayush-jain2002 <109455029+ayush-jain2002@users.noreply.github.com>
2022-11-10 23:21:44 +0530

    Time: 74 ms (77.25%), Space: 15.9 MB (47.35%) - LeetHub

---
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        vector<double>ans;
+        multiset<int>s(begin(nums),begin(nums)+k);
+        
+        auto mid=next(s.begin(),k/2);
+        for(int i=k;;i++){
+            ans.push_back((double(*mid)+*prev(mid,1-k%2))/2.0);
+            if(i==nums.size()) return ans;
+            s.insert(nums[i]);
+            if(nums[i]<*mid){
+                mid--;
+            }
+            if(nums[i-k]<=*mid) mid++;
+            s.erase(s.lower_bound(nums[i-k]));
+        }
+        return ans;
+    }
+};
